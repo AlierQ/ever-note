b7e40cd5c28d2b428818247b4c55c86db4cad68a
AlierQ <1029369938@qq.com>
2022-08-07 16:33:03 +0800

    create deploy.sh and publish preview to Github Page

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+rm -f dist &&
+yarn build &&
+cd dist &&
+git init &&
+git add . &&
+git commit -m "commit page" &&
+git branch -m main &&
+git remote add origin git@github.com:AlierQ/evernote-page.git &&
+git push -u -f origin main &&
+cd -
