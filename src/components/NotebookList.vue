8f2ba2d8ea9baeb362d7b2eea4ae126a11c4f61f
AlierQ <1029369938@qq.com>
2022-08-08 23:21:11 +0800

    complete notebooks  style

--- src/components/NotebookList.vue
+++ src/components/NotebookList.vue
@@ -1,14 +1,46 @@
 <template>
   <div id="notebook-list">
-    <h1>{{ msg }}</h1>
-    <ul>
-      <li>
-        <router-link to="/note/1">笔记本1</router-link>
-      </li>
-      <li>
-        <router-link to="/note/2">笔记本2</router-link>
-      </li>
-    </ul>
+    <header>
+      <a href="javascript:void(0);">
+        <plus theme="outline" size="22" fill="#a2a2a2" :strokeWidth="3" />
+        <span>新建笔记本</span>
+      </a>
+    </header>
+    <main>
+      <h3>笔记本列表(10)</h3>
+      <div class="notebook-list">
+        <a href="#" class="notebook">
+          <div>
+            <notebook-one
+              theme="outline"
+              size="24"
+              fill="#4a4a4a"
+              :strokeWidth="3"
+            />
+            <span>笔记本</span>
+            <span><span>3</span></span>
+            <span>3天前</span>
+            <span>编辑</span>
+            <span>删除</span>
+          </div>
+        </a>
+        <a href="#" class="notebook">
+          <div>
+            <notebook-one
+              theme="outline"
+              size="24"
+              fill="#4a4a4a"
+              :strokeWidth="3"
+            />
+            <span>笔记本</span>
+            <span><span>12</span></span>
+            <span>2022/8/8</span>
+            <span>编辑</span>
+            <span>删除</span>
+          </div>
+        </a>
+      </div>
+    </main>
   </div>
 </template>
 
@@ -16,6 +48,7 @@
 import { ref } from "vue";
 import { useRouter } from "vue-router";
 import Auth from "@/api/auth";
+import { Plus, NotebookOne } from "@icon-park/vue-next";
 
 const router = useRouter();
 
@@ -29,7 +62,5 @@ const msg = ref("笔记本列表");
 </script>
 
 <style scoped lang="less">
-h1 {
-  color: red;
-}
+@import "@/assets/style/notebook-list.less";
 </style>
