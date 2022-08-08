918caa4f1a23825135bfbfee2f726ef0b650b58c
AlierQ <1029369938@qq.com>
2022-08-08 15:57:40 +0800

    use computed compute request username

--- src/components/Avatar.vue
+++ src/components/Avatar.vue
@@ -1,9 +1,29 @@
 <template>
-  <span :title="user.username">{{ user.username.slice(0, 1) }}</span>
+  <span :title="username">{{ slug }}</span>
 </template>
 <script setup lang="ts">
-import { ref } from "vue";
-const user = ref({ username: "AlierQ" });
+import { ref, computed } from "vue";
+import Auth from "@/api/auth";
+const username = ref("");
+
+// setup中使用计算属性
+const slug = computed(() => {
+  return username.value.slice(0, 1);
+});
+
+// 请求数据数据，理论上写在created中，但是setup中没有必要使用created
+Auth.get_login_state().then((res) => {
+  type State = {
+    isLogin: boolean;
+    data?: {
+      username: string;
+      createAt: string;
+      updatedAt: string;
+    };
+  };
+
+  if ((res as State).isLogin) username.value = (res as State).data!.username;
+});
 </script>
 <style scoped lang="less">
 span {
