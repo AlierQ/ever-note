ec8ecb5ce0f42c6f03c6ad66a709c7db0ad88930
AlierQ <1029369938@qq.com>
2022-08-08 17:09:27 +0800

    install and use pinia

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,10 @@ import "@icon-park/vue-next/styles/index.css";
 import "@/style.css";
 import App from "@/App.vue";
 import router from "@/router";
-
+import { createPinia } from "pinia";
 const app = createApp(App);
+const pinia = createPinia();
 
 app.use(router);
-
+app.use(pinia);
 app.mount("#app");
