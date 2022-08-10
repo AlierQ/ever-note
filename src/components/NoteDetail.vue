795b7c3011cba106d413ddbae43d0feebc17ced3
AlierQ <1029369938@qq.com>
2022-08-10 20:56:17 +0800

    write NoteDetail edit district style

--- src/components/NoteDetail.vue
+++ src/components/NoteDetail.vue
@@ -2,25 +2,53 @@
   <div id="note" class="detail">
     <NoteSidebar></NoteSidebar>
     <div class="note-detail">
-      <h1>{{ msg }} : {{ $route.query.notebookId }}</h1>
-      <h1>noteId : {{ $route.query.noteId }}</h1>
-      <!-- <div class="note-empty">请选择笔记</div>
-      <div class="note-detail-ct">
-        <div class="note-bar">
-          <span> 创建日期: {{ 123123 }}</span>
-          <span> 更新日期: {{ 123123123 }}</span>
-          <span> {{ 12312321 }}</span>
-          <span class="iconfont icon-delete"></span>
-          <span class="iconfont icon-fullscreen"></span>
+      <div class="note-detail">
+        <div class="note-empty" v-show="">请选择笔记</div>
+        <div class="note-detail-ct" v-show="true">
+          <div class="note-bar">
+            <span> 创建日期: {{ currentNote.createdAt }}</span>
+            <span> 更新日期: {{ currentNote.updatedAt }}</span>
+            <span> {{ currentNote.stateText }}</span>
+            <span class="icon-fullscreen" @click="togglePreview">
+              <preview-close
+                v-show="!preview"
+                theme="outline"
+                size="18"
+                fill="#aaa"
+                :strokeWidth="3"
+              />
+              <preview-open
+                v-show="preview"
+                theme="outline"
+                size="18"
+                fill="#aaa"
+                :strokeWidth="3"
+              />
+            </span>
+            <span
+              ><delete-one
+                theme="outline"
+                size="18"
+                fill="#aaa"
+                :strokeWidth="3"
+            /></span>
+          </div>
+          <div class="note-title">
+            <input
+              type="text"
+              placeholder="输入标题"
+              v-model="currentNote.title"
+            />
+          </div>
+          <div class="editor">
+            <textarea
+              placeholder="输入内容, 支持 markdown 语法"
+              v-model="currentNote.content"
+            ></textarea>
+            <div class="preview markdown-body"></div>
+          </div>
         </div>
-        <div class="note-title">
-          <input type="text" placeholder="输入标题" />
-        </div>
-        <div class="editor">
-          <textarea placeholder="输入内容, 支持 markdown 语法"></textarea>
-          <div class="preview markdown-body"></div>
-        </div>
-      </div> -->
+      </div>
     </div>
   </div>
 </template>
@@ -30,14 +58,28 @@ import { ref } from "vue";
 import { useRouter } from "vue-router";
 import Auth from "@/api/auth";
 import NoteSidebar from "@/components/NoteSidebar.vue";
+import { DeleteOne, PreviewClose, PreviewOpen } from "@icon-park/vue-next";
 const router = useRouter();
+
+const preview = ref(false);
+
+const currentNote = ref({
+  title: "未命名笔记",
+  content: "这里是内容",
+  createdAt: "1天前",
+  updatedAt: "刚刚",
+  stateText: "未保存",
+});
+
+const togglePreview = () => {
+  preview.value = !preview.value;
+};
 
 Auth.get_login_state().then((ref: any) => {
   if (!ref.isLogin) {
     router.push("/login");
   }
 });
-const msg = ref("notebookId");
 </script>
 
 <style scoped lang="less">
